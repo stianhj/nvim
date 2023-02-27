f5db394b5501f8a9fc795c748d2bf5359f41eddd
Stian H. Johannesen <stianhj@gmail.com>
2023-02-27 18:07:21 +0100

    add cmp mappings

--- init.lua
+++ init.lua
@@ -170,5 +170,13 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'buffer' },
-  })
+  }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
 })
